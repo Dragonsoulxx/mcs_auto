6079a37ac4fa08d564d19089099887171498361b
Dragonsoulxx <127168702+Dragonsoulxx@users.noreply.github.com>
2025-03-08 15:52:59 +0100

    Delete multics_exchanger directory

--- multics_exchanger/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-session_start();
-session_destroy();
-header("Location: index.php?lang=" . (isset($_GET['lang']) && in_array($_GET['lang'], ['it', 'en']) ? $_GET['lang'] : 'it'));
-exit();
-?>
